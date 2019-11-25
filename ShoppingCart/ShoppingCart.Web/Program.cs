4607ba4c39e4ad2b3e93a5e16f6ae3788359d31e
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-11-25 10:51:01 +0530

    Unnecessary Comment Removed

--- ShoppingCart/ShoppingCart.Web/Program.cs
+++ ShoppingCart/ShoppingCart.Web/Program.cs
@@ -14,20 +14,12 @@ namespace ShoppingCart.Web
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
             BuildWebHost(args).Run();
         }
 
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
-
-        public static IWebHost BuildWebHost(string[] args) =>
-    WebHost.CreateDefaultBuilder(args)
-        .UseStartup<Startup>()
-        .Build();
+        public static IWebHost BuildWebHost(string[] args) => 
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
     }
 }
